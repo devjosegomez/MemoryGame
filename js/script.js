9c618efa6698255fd18bd7b41d6586fff2e58eca
devjosegomez <devjosegomez@gmail.com>
2020-01-10 18:57:19 -0600

    startGame()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var titleImages = []
+var startBtn = document.getElementById("start")
+
+startBtn.addEventListener("click", startGame)
 
 for(x = 1; x < 7; x++){
     titleImages.push(x+".jpg");
 }
 
+function startGame(){
+    console.log("clicked")
+}
